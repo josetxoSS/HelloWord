840614015810346e3285341fc4e2f3e0c24dbcf0
jose sanchez <txami6996@gmail.com>
2024-03-17 21:44:12 +0100

    first commit

--- hi.py
+++ hi.py
@@ -1,7 +1,3 @@
 # My first commit
-print("Hi bb")
-# Update commit
-print("I made a mistake")
-#git branch -r
-#git fetch origin main ### git fetch <remote name> <branch name>
-print("updating the  file from git")
+print("Him  Goku")
+
